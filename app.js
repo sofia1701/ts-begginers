c8557d2c42d61e77f968fb37233c85fdb099e61e
Sofia <sofia.dionisio94@gmail.com>
2020-12-10 15:47:09 +0000

    Working with booleans

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //Numbers 
-function add(num1, num2) {
+function add(num1, num2, showResult) {
+    if (showResult) {
+        console.log(num1 + num2);
+    }
     return num1 + num2;
 }
 var number1 = 3;
 var number2 = 4.4;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+add(number1, number2, printResult);
--- app.ts
+++ app.ts
@@ -1,12 +1,15 @@
-//Numbers 
+//Numbers, Booleans
 
-function add(num1: number, num2: number) {
+function add(num1: number, num2: number, showResult: boolean) {
+  if(showResult) {
+    console.log(num1 + num2)
+  }
   return num1 + num2
 }
 
 const number1 = 3;
 const number2 = 4.4;
+const printResult = true;
 
-const result = add(number1, number2);
+add(number1, number2, printResult);
 
-console.log(result);
