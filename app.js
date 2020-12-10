d2c362140047b98377c5b061d7c3cff7084ccaa3
Sofia <sofia.dionisio94@gmail.com>
2020-12-10 15:33:04 +0000

    Working with numbers

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("it is working");
+//Numbers 
+function add(num1, num2) {
+    return num1 + num2;
+}
+var number1 = 3;
+var number2 = 4.4;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("it is working")
+//Numbers 
+
+function add(num1: number, num2: number) {
+  return num1 + num2
+}
+
+const number1 = 3;
+const number2 = 4.4;
+
+const result = add(number1, number2);
+
+console.log(result);
