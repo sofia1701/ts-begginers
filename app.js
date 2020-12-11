790c2a7effc9ac58d437db2768e42e5f8727e2a2
Sofia <sofia.dionisio94@gmail.com>
2020-12-11 14:39:57 +0000

    Working with tupples

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-//Objects, arrays
+//Objects, arrays, tupples
 var person = {
     name: "Sofia",
     age: 26,
-    hobbies: ['sports', 'cinema', 'travel']
+    hobbies: ['sports', 'cinema', 'travel'],
+    role: [2, 'author']
 };
+// person.role.push('admin'); push is an exeption, typescript can't catch error
+// person.role[1] = 10; gives an error as tupple only accepts 2 elements of type number and string
+person.role = [3, 'admin'];
 var favouriteHobbies;
 favouriteHobbies = ['sports'];
-console.log(person.name);
+console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby.toUpperCase());
 }
+;
--- app.ts
+++ app.ts
@@ -1,15 +1,25 @@
-//Objects, arrays
+//Objects, arrays, tupples
 
-const person = {
+const person: {
+  name: string,
+  age: number,
+  hobbies: string[],
+  role: [number, string]
+} = {
   name: "Sofia",
   age: 26,
-  hobbies: ['sports', 'cinema', 'travel']
+  hobbies: ['sports', 'cinema', 'travel'],
+  role: [2, 'author']
 };
+
+// person.role.push('admin'); push is an exeption, typescript can't catch error
+// person.role[1] = 10; gives an error as tupple only accepts 2 elements of type number and string
+person.role = [3, 'admin'];
 
 let favouriteHobbies: string[];
 favouriteHobbies = ['sports'];
 
-console.log(person.name);
+console.log(person);
 
 for(const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
