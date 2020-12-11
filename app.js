6748cd57e2e06118da5100c1bbc7578e80eabb69
Sofia <sofia.dionisio94@gmail.com>
2020-12-11 14:54:21 +0000

    Working with enums

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Objects, arrays, tupples
+//Objects, arrays, tuples
 var person = {
     name: "Sofia",
     age: 26,
@@ -6,13 +6,29 @@ var person = {
     role: [2, 'author']
 };
 // person.role.push('admin'); push is an exeption, typescript can't catch error
-// person.role[1] = 10; gives an error as tupple only accepts 2 elements of type number and string
+// person.role[1] = 10; gives an error as tuple only accepts 2 elements of type number and string
 person.role = [3, 'admin'];
 var favouriteHobbies;
 favouriteHobbies = ['sports'];
-console.log(person);
+//console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+    //console.log(hobby.toUpperCase());
 }
 ;
+//Enums
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person1 = {
+    name: "Sofia",
+    age: 26,
+    hobbies: ['sports', 'cinema', 'travel'],
+    role: Role.ADMIN
+};
+if (person1.role === Role.AUTHOR) {
+    console.log('The role is author');
+}
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-//Objects, arrays, tupples
+//Objects, arrays, tuples
 
 const person: {
   name: string,
@@ -13,14 +13,29 @@ const person: {
 };
 
 // person.role.push('admin'); push is an exeption, typescript can't catch error
-// person.role[1] = 10; gives an error as tupple only accepts 2 elements of type number and string
+// person.role[1] = 10; gives an error as tuple only accepts 2 elements of type number and string
 person.role = [3, 'admin'];
 
 let favouriteHobbies: string[];
 favouriteHobbies = ['sports'];
 
-console.log(person);
+//console.log(person);
 
 for(const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+  //console.log(hobby.toUpperCase());
 };
+
+//Enums
+
+enum Role { ADMIN, READ_ONLY, AUTHOR }
+
+const person1 = {
+  name: "Sofia",
+  age: 26,
+  hobbies: ['sports', 'cinema', 'travel'],
+  role: Role.ADMIN,
+};
+
+if(person1.role === Role.AUTHOR) {
+  console.log('The role is author')
+}
